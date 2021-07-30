8ad563dc107bf3584cac9b4b6b1ca80a98104b80
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-07-30 17:29:37 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa3c943069041bf9a28723b941499dc2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ff11a96d-e7d4-4a64-8d68-89f5712d7a5e'
+};
